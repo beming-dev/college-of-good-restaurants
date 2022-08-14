f534607a25f032c70f3c864eb76a19e5a37a4007
unknown <alsrhks0503@office.uos.ac.kr>
2022-08-14 21:15:52 +0900

    Refactor index

---
+++ lib/types.ts
@@ -0,0 +1,10 @@
+export interface collegeInfoType {
+  college_name: string;
+  number_of_students: number;
+  college_id: number;
+}
+
+export interface windowSizeType {
+  width: number | undefined;
+  height: number | undefined;
+}
